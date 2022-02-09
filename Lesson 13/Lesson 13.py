37e4e58be8ee8742d3225994716b0a4ed071d527
Vopernaqu <84228313+Vopernaqu@users.noreply.github.com>
2022-02-09 18:28:35 +0200

    Ls13

---
+++ Lesson 13/Lesson 13.py
@@ -0,0 +1,93 @@
+# #1
+# from email.policy import default
+# from re import I
+
+
+# search_systems = {
+#                 "Google" : 82.8,
+#                 "Yahoo" : 6.42,
+#                 "Baidu" : 4.89,
+#                 "Bing" : 3.91,
+#                 "Ask" : 0.52
+#                 }
+# for a, b in search_systems.items() :
+#     print(f"{a} - {b}%")
+# print("-"*50)
+# #2
+# good_task = {
+#             "task1" : "покормить собаку",
+#             "task2" : "погулять с собакой",
+#             "task3" : "помочь друзьям"
+#             }
+# for c in good_task.values() :
+#     print(f"Я собираюсь {c}")
+# print("-"*50)
+# #3
+# users = {
+#         "u1" : "Egor",
+#         "u2" : "Artem",
+#         "u3" : "Max",
+#         "u4" : "Ivan"
+#         }
+# for d in users.keys() :
+#     print(f"Привіт {d}")
+# print("-"*50)
+# #4
+# users_items = {
+#               "wood" : 23,
+#               "stone" : 56,
+#               "pickaxe" : 1,
+#               "axe" : 1,
+#               "maps" : 10
+#               }
+# for f, h in users_items.items() :
+#     print(f"У персонажа есть {f} в кол-ве {h}")
+# print("-"*50)
+# #5
+# n = int(input("Введите n: "))
+# numbers = {}
+
+# u = 1
+# while u <= n :
+#     numbers[u] = u ** 2
+#     u += 1
+
+# print(numbers)
+# print("-"*50)
+# #6
+# genres_of_pc_games = {
+#                      "Головоломка" : "Portal 1/2",
+#                      "Симулятор" : "Microsoft Flight Simulator",
+#                      "Стратегия" : "Hearts of Iron 4"
+#                      }
+# for l, o in genres_of_pc_games.items() :
+#     print(f"{l} - {o}")
+# print("-"*50)
+#7
+
+
+# famous_programmers = {
+#                     "Ада Лавлейс" : "10/12/1815",
+#                     "Марк Цукерберг" : "14/05/1984",
+#                     "Билл Гейтс" : "28/10/1955"
+#                     }
+# for i, k in famous_programmers.items() :
+#     print(f"{i} родился(ась) {k}")
+#print("-"*50)
+#8
+#food = {
+#        "Отбивные" : "Курица",
+#        "Пюрешка" : "Картошка",
+#        "Каша" : "Крупа"
+#        } 
+#n = input()
+#for i, u in food.items() :
+#    if n != i :
+#        print("В холодосе такого нет")
+#        break
+#    elif n == i :
+#        print(f"{n} - {u}")
+#        break
+#print("-"*50)
+
+    
