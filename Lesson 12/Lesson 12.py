999613350c725801976351318ea635cc8ffbf0b7
Vopernaqu <84228313+Vopernaqu@users.noreply.github.com>
2022-02-12 16:18:03 +0200

    Ls12

---
+++ Lesson 12/Lesson 12.py
@@ -0,0 +1,19 @@
+#user = {"u1" : "human"
+        #"u2" : "superhero"
+        #"u3" : "wolf"
+        #"u4" : "Brocolu"
+        #}
+#print(user)
+#print(user["u3"])
+#user["u5"] = "Programmer"
+#print(user)
+user = {}
+user["u1"] = "superhero"
+user["u2"] = "human"
+user["u3"] = "brocoli"
+
+print(user)
+
+user["u2"] = "wolf"
+
+print(user)
